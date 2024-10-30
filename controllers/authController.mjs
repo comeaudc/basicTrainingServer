c1d11f6f23e9095bb52df49d65252b731fae38ee
DylanC <88355961+comeaudc@users.noreply.github.com>
2024-10-30 19:02:47 -0400

    Added Login Route

--- controllers/authController.mjs
+++ controllers/authController.mjs
@@ -1,7 +1,10 @@
 import User from '../models/userSchema.mjs';
-
+import { validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+import dotenv from 'dotenv';
 // async function findUserById
-async function findUserById(req, res) {
+async function findUserByToken(req, res) {
   try {
     const user = await User.findById(req.user.id).select('-password');
 
@@ -12,4 +15,47 @@ async function findUserById(req, res) {
   }
 }
 
-export default { findUserById };
+async function loginUser(req, res) {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    res.status(400).json({ errors: errors.array() });
+  }
+
+  const { email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      res.status(400).json({ msg: 'Invalid Credentials' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      res.status(400).json({ msg: 'Invalid Credentials' });
+    }
+
+    const payload = {
+      user: {
+        id: user._id,
+      },
+    };
+
+    jwt.sign(
+      payload,
+      process.env.jwtSecret,
+      { expiresIn: 3600 },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ msg: 'Server Error' });
+  }
+}
+
+export default { findUserByToken, loginUser };
--- routes/authRoutes.mjs
+++ routes/authRoutes.mjs
@@ -1,8 +1,18 @@
 import express from 'express';
 import authCTRL from '../controllers/authController.mjs';
 import auth from '../middleware/auth.mjs';
+import { check } from 'express-validator';
 const router = express.Router();
 
-router.route('/').get(auth, authCTRL.findUserById);
+router
+  .route('/')
+  .get(auth, authCTRL.findUserByToken)
+  .post(
+    [
+      check('email', 'Please include a valid email'),
+      check('password', 'Password Required'),
+    ],
+    authCTRL.loginUser
+  );
 
 export default router;
