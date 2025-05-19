eb78d6e72727858aaaf08351f0a98bf9d81a61a9
Dylan Comeau <comeaudc@comcast.net>
2025-05-19 16:06:54 -0400

    Fixed some error messages beings passed to the frontend

--- controllers/authController.mjs
+++ controllers/authController.mjs
@@ -28,13 +28,13 @@ async function loginUser(req, res) {
     let user = await User.findOne({ email });
 
     if (!user) {
-      return res.status(400).json({ msg: "Invalid Credentials" });
+      return res.status(400).json({ errors: [{ msg: "Invalid Credentials" }] });
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
 
     if (!isMatch) {
-       return res.status(400).json({ msg: "Invalid Credentials" });
+      return res.status(400).json({ errors: [{ msg: "Invalid Credentials" }] });
     }
 
     const payload = {
